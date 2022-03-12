d8a8dfee29d565d22f77c89da6de5caf30138ea1
unknown <tkdgns25300@gmail.com>
2022-03-12 12:12:36 +0900

    upload a image API작성

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const cloudinary = require("cloudinary");
 const auth = require("../middleware/auth");
 const authAdmin = require("../middleware/authAdmin");
-const fs = require('fs');
+const fs = require("fs");
 
 // we will upload image on cloudinary
 cloudinary.config({
@@ -18,7 +18,6 @@ router.post("/upload", (req, res) => {
       return res.status(400).send("No files were uploaded.");
 
     const file = req.files.file;
-    console.log(file)
     if (file.size > 1024 * 1024) {
       removeTmp(file.tempFilePath);
       return res.status(400).json({ msg: "Size too large" });
@@ -29,21 +28,34 @@ router.post("/upload", (req, res) => {
       return res.status(400).json({ msg: "File format is incorrect." });
     }
 
-    cloudinary.v2.uploader.upload(file.tempFilePath, {folder: "test"}, async(err, result) => {
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: "test" },
+      async (err, result) => {
         if (err) throw err;
-
-        res.json({ public_id: result.public_id, url: result.secure_url })
-    })
-
+        removeTmp(file.tempFilePath);
+        res.json({ public_id: result.public_id, url: result.secure_url });
+      }
+    );
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
 });
 
+// Delete image
+router.post("/destroy", (req, res) => {
+  try {
+    
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+})
+
+// Tmp 파일 삭제 함수
 const removeTmp = (path) => {
-    fs.unlink(path, err => {
-        if (err) throw err;
-    })
-}
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
+};
 
 module.exports = router;
