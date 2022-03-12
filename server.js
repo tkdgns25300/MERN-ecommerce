8f3fd56b98b7c1cc8f2d5647428a08ebb1dd6b6a
unknown <tkdgns25300@gmail.com>
2022-03-12 13:18:33 +0900

    update a product API작성

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(fileUpload({
 app.use('/user', require('./routes/userRouter'));
 app.use('/api', require('./routes/categoryRouter'));
 app.use('/api', require('./routes/upload'));
+app.use('/api', require('./routes/productRouter'));
 
 
 // Connect to mongodb
